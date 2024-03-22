910779a1d4c0f5126af14d1f9ab80a3f4069e601
Robert Nersesyan <r.nersesyan@instadev.net>
2024-03-22 18:27:05 +0400

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Robert Nersesyan on 22.03.24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
